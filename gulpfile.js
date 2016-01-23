152ea2d139a61c5a9bbd21d71f43224fde08c411
Robin Hedin <robin.hedin@gmail.com>
2016-01-23 17:15:09 +0100

    Added gulp foundation

---
+++ gulpfile.js
@@ -0,0 +1,15 @@
+var gulp = require('gulp');
+var less = require('gulp-less');
+var watch = require('gulp-watch');
+var util = require('gulp-util');
+
+gulp.task('less', function() {
+    return gulp.src('app/style/less/base.less')
+      .pipe(less().on('error', util.log))
+      .pipe(gulp.dest('app/style/generated_css'))
+});
+gulp.task('watch', function() {
+    gulp.watch('app/style/less/*.less', ['less']);  // Watch all the .less files, then run the less task
+});
+
+gulp.task('default', ['watch']); // Default will run the 'entry' watch task
